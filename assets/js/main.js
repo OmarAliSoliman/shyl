e29762aa524de383910670c41480be8fe902c5f7
OmarAliSoliman <omarasmar9955@gmail.com>
2020-10-01 14:34:17 +0200

    change-bootstrap-owl-versions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,27 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
   "use strict";
 
   AOS.init();
-
-  var navBar = $('.uppernav .navbar');
-  $(window).scroll(()=>{
-    if($(this).scrollTop() > 10){
-      $(navBar).addClass('fixed-navbar');
-    }else{
-      $(navBar).removeClass('fixed-navbar');
-    }
-  })
-
-  if($('.header-slider').length){
-    $('.header-slider').slick({
-      arrows: true,
-      rtl: true,
-    })
-  }
-
-  if($('.screen-slider').length){
-    $('.screen-slider').slick({
-      arrows: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      dots:false,
-      rtl: true,
-      // centerMode: true,
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          // centerMode: true,
-  
-        }
-  
-      }, {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          dots: false,
-          infinite: true,
-  
-        }
-      },  {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: false,
-          infinite: true,
-          autoplay: true,
-          autoplaySpeed: 2000,
-        }
-      }]
-    });
-  }
-})
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    // margin: 10,
+    responsiveClass: true,
+    nav:false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+});
